10cb02a2837bb7af7ccb3ec82dafe411db183b9a
Andrew Houts <16907671+ahouts@users.noreply.github.com>
2019-10-24 22:27:11 -0700

    Update caltraind.rb

--- Formula/caltraind.rb
+++ Formula/caltraind.rb
@@ -2,8 +2,8 @@ class Caltraind < Formula
   desc "notification generator for caltrain departures"
   homepage "https://github.com/ahouts/caltraind"
 
-  url "https://github.com/ahouts/caltraind/releases/download/v0.2.1/caltraind-osx-x86_64.tar.gz"
-  sha256 "994fa51e90a6604f762ba5fb6f6c8e5eeca6b54795c48d3cb6f86caf364b6357"
+  url "https://github.com/ahouts/caltraind/releases/download/v0.2.2/caltraind-osx-x86_64.tar.gz"
+  sha256 "8879516e53c0e76a047e316ddb197c2bd1a13dab6381387b5bbaabc8a14fc56a"
 
   def install
     bin.install "caltraind"
